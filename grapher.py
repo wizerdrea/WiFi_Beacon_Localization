56f93fef896117de9f5f176068544a0b2fb8801c
wizerdrea <55115849+wizerdrea@users.noreply.github.com>
2022-04-20 14:44:59 -0600

    Update grapher.py

--- grapher.py
+++ grapher.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 import json
 
-runs_to_plot = ["run1.csv", "run2.csv", "run3.csv", "run4.csv", "run5.csv", "run6.csv", "run7.csv"]
+runs_to_plot = [("run1.csv", 15.2, 24.4, "red", "Loc 1"), ("run2.csv", 29, 27.4, "blue", "Loc 2"), ("run3.csv", 24.4, 33.5, "green", "Loc 3"), ("run4.csv", 0, 21.3, "pink", "Loc 4"), ("run5.csv", 0, 0, "grey", "Loc 5"), ("run6.csv", 45.7, 0, "orange", "Loc 6"), ("run7.csv", 45.7, 27.4, "purple", "Loc 7")]
 OUT_FILE = "graph.png"
 X_LIMITS = [73.152,-15.24]
 Y_LIMITS = [-21.336, 42.672]
@@ -11,7 +11,9 @@ plt.figure(0)
 
 for run in runs_to_plot:
 
-    run_data = list(csv.reader(open(run)))
+    plt.plot(run[1],run[2],'*', color=run[3])
+    
+    run_data = list(csv.reader(open(run[0])))
 
     t = []
     x = []
@@ -22,21 +24,11 @@ for run in runs_to_plot:
         x.append(float(line[1]))
         y.append(float(line[2]))
 
-    plt.plot(x,y,'o')
-
-with open("beacon_map.json", "r") as file:
-    beacon_map = json.load(file)
+    plt.plot(x,y,'o', color=run[3], label=run[4])
     
-beacon_x = []
-beacon_y = []
-
-for value in beacon_map.values():
-    beacon_x.append(value["x"])
-    beacon_y.append(value["y"])
-
-plt.plot(beacon_x, beacon_y, 'o')
 
 plt.xlim(X_LIMITS)
 plt.ylim(Y_LIMITS)
+plt.legend(loc="upper left")
 plt.savefig(OUT_FILE)
 plt.show()
